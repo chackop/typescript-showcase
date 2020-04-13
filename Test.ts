561001a0c223925fa58e36677c08ac85aaf5b13c
Chacko <chackop@gmail.com>
2020-04-13 12:02:30 +0100

    applied standard animal

---
+++ Test.ts
@@ -0,0 +1,37 @@
+// Declaring a class
+class Animal {
+    name: string;
+    constructor(theName: string){
+        this.name = theName;
+    }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}`);
+    }
+}
+
+// Inheriting from the class
+class Snake extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
+    move(distanceInMeters = 5){
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
+    move(distanceInMeters = 45){
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy");
+let tom: Animal = new Horse("Tommy");
+
+sam.move();
+tom.move(34);
